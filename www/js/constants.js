97bdcbfe411f6a39a20c0634bcca34611d74edbd
ying07.liu <ying07.liu@vipshop.com>
2015-12-28 16:17:55 +0800

    1111111

--- www/js/constants.js
+++ www/js/constants.js
@@ -2,6 +2,7 @@ angular.module('common.constants', [])
 
 .constant('AppConstant', {
 	'APP_NAME':'oxostore',
-	'BASE_URL':'https://yklis.vicp.net:8443/YkAPI/service'
+	'BASE_URL':'https://211.97.0.5:8443/YkAPI/service'
+	//'https://yklis.vicp.net:8443/YkAPI/service'
 	//'http://211.97.0.5:8080/YkAPI/service'
 });
