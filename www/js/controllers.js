d62e182c89c98b88af008a7a5a14e966e02f3c96
ying07.liu <ying07.liu@vipshop.com>
2016-02-25 16:26:23 +0800

    echarts应用

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -251,16 +251,44 @@ angular.module('starter.controllers', [])
 
 .controller('AccountCtrl', function($scope,$http,$ionicLoading,$ionicPopup,$timeout, $q,AppConstant,$ionicHistory,Chats,$state,CommonService) {
 
+    //echarts应用start
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    // 使用指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    //echarts应用stop
+
   $scope.datetimeValue = new Date();
   
-  var getVersionNumber = function() {
+  /*var getVersionNumber = function() {
 
     cordova.getAppVersion.getVersionNumber().then(function (version) {
       $scope.VersionNumber = version;
     });
   };
 
-  getVersionNumber();
+  getVersionNumber();*/
 
   //生成二维码start
   $scope.qrText = {srcQrText : ""};
