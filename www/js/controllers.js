2aba28372c55469eeaf041c9d19e2da92cc1f7c4
ying07.liu <ying07.liu@vipshop.com>
2016-03-25 19:56:23 +0800

    1111111111

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,11 @@
+;(function() {
+    //设立"严格模式"的目的
+    //1、消除Javascript语法的一些不合理、不严谨之处，减少一些怪异行为;
+    //2、消除代码运行的一些不安全之处，保证代码运行的安全；
+　　//3、提高编译器效率，增加运行速度；
+　　//4、为未来新版本的Javascript做好铺垫
+    "use strict";
+
 angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope,$ionicLoading,$ionicPopup,$timeout,$state) {
 
@@ -8,7 +16,6 @@ angular.module('starter.controllers', [])
         $scope.$apply(function(){
             //$scope.$apply更新该$scope.enCryptSmsBody到view
             $scope.smsList = success;
-            vSmsList = success;
         });         
       },
       function(errorStr) {
@@ -226,7 +233,7 @@ angular.module('starter.controllers', [])
     if(!passWord || passWord == '') passWord = '';
         
     var requestPwd = encryptedString(rsaPubKey,passWord);
-    params = {
+    var params = {
         methodNum:"login",
         userId:$scope.login.userId,
         passWord:requestPwd
@@ -489,3 +496,5 @@ angular.module('starter.controllers', [])
   };
 
 });
+
+})();
