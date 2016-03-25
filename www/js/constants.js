7d41da4c2317ad446e93f0173a2aeddd206a5025
ying07.liu <ying07.liu@vipshop.com>
2016-03-25 14:12:36 +0800

    1111111111

--- www/js/constants.js
+++ www/js/constants.js
@@ -2,7 +2,8 @@ angular.module('common.constants', [])
 
 .constant('AppConstant', {
 	'APP_NAME':'oxostore',
-	'BASE_URL':'https://211.97.0.5:8443/YkAPI/service'
+	//https用手机访问没有任何问题，用浏览器调试时，需要先在浏览器地址中访问下，手动信任。为了省去浏览器调试时的麻烦，用http访问
+	//'BASE_URL':'https://211.97.0.5:8443/YkAPI/service',
+	'BASE_URL':'http://211.97.0.5:8080/YkAPI/service'
 	//'https://yklis.vicp.net:8443/YkAPI/service'
-	//'http://211.97.0.5:8080/YkAPI/service'
 });
