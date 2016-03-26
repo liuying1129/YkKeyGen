69bcab139b70df17d6adeb08d4170f06c79d3974
ying07.liu <ying07.liu@vipshop.com>
2016-03-26 19:10:19 +0800

    IIFE、严格模式

--- www/js/commonInterceptor.js
+++ www/js/commonInterceptor.js
@@ -1,3 +1,11 @@
+;(function() {
+	//设立"严格模式"的目的
+	//1、消除Javascript语法的一些不合理、不严谨之处，减少一些怪异行为;
+	//2、消除代码运行的一些不安全之处，保证代码运行的安全；
+	//3、提高编译器效率，增加运行速度；
+	//4、为未来新版本的Javascript做好铺垫
+	"use strict";
+	
 angular.module('starter.interceptor', [])
 
 .factory('Interceptor', function() {
@@ -54,4 +62,6 @@ angular.module('starter.interceptor', [])
 	};
 
 	return myInterceptor;
-});	
+});
+
+})();
